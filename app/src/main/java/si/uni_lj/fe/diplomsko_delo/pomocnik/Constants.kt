9cdc6a50eb60fb6c5dcda76e44ee70c1b7a7022c
Eva Urankar <eva.urankar@mmv.si>
2025-05-16 01:36:34 +0200

    adding our own model

--- app/src/main/java/si/uni_lj/fe/diplomsko_delo/pomocnik/Constants.kt
+++ app/src/main/java/si/uni_lj/fe/diplomsko_delo/pomocnik/Constants.kt
@@ -4,9 +4,16 @@ package si.uni_lj.fe.diplomsko_delo.pomocnik
  * Application-wide constants for model paths and configuration.
  */
 object Constants {
-    const val YOLO_PATH = "yolov8n-oiv7_float32.tflite"
-    const val LABELS_PATH_SI = "oznake.txt"
-    const val LABELS_PATH_EN = "labels.txt"
+    // Uncomment to use 601 classes from openimages, ultralytics model
+    //const val YOLO_PATH = "yolov8n-oiv7_float32.tflite"
+    //const val LABELS_PATH_SI = "oznake.txt"
+    //const val LABELS_PATH_EN = "labels.txt"
+
+    // our model:
+    const val YOLO_PATH = "our_float32.tflite"
+    const val LABELS_PATH_SI = "oznake_new.txt"
+    const val LABELS_PATH_EN = "labels_new.txt"
+
     const val MIDAS_PATH = "midas.tflite"
     const val SCENE_DETECTOR_PATH = "resnet18_places365_float32.tflite"
     const val SCENE_LABELS_PATH_EN = "places365_labels_en.txt"
